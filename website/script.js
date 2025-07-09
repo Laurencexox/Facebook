4f19bfd1a4822c5a4a93d598174316772a8300b4
Laurencexox <sanjuanjay17@gmail.com>
2025-07-09 12:12:49 +0800

    Add files via upload

---
+++ website/script.js
@@ -0,0 +1,14 @@
+function sayHello() {
+  alert("Hello from your website!");
+}
+
+function playMusic() {
+  const audio = document.getElementById("myAudio");
+  audio.play();
+}
+
+function showFlower() {
+  const popup = document.getElementById('flowerPopup');
+  popup.classList.add('show');
+}
+
